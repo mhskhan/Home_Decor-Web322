2e921aa16538bf134681cf6c729faaa1cb46a8e3
Md Hasin Shadab Khan <mdhasinshadabkhan@gmail.com>
2025-05-05 21:24:02 -0400

    Updated

--- server.js
+++ server.js
@@ -262,10 +262,7 @@ app.use(function (err, req, res, next) {
 });
 
 // MongoDB Connect
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Connected to MongoDB");
 }).catch(err => {
   console.log("Can't connect to the MongoDB:", err);
